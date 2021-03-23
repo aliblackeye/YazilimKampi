aec571c6aed74cf9a61aaf15efe91944141da9fa
aliblackeye <ali.blackeye@hotmail.com>
2021-03-23 23:54:04 +0300

    23.03.2021

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<Type>
+    {
+        Type[] items;
+        //constructor
+        public MyList()
+        {
+            items = new Type[0];
+        }
+        public void Add(Type item)
+        {
+            Type[] tempArray = items;
+            items = new Type[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
